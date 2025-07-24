d831238d42d1e77b0a1a89ced4df6b754c9d35f3
LeonM95 <leomasis95@gmail.com>
2025-07-23 20:14:55 -0600

    created project, added navbar, added hero section

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import StarBackground from "../components/StarBackground";
+import { ThemeToggle } from "../components/ThemeToggle";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* The toggle */}
+      <ThemeToggle />
+      {/* backgroud effects */}
+      <StarBackground />
+      {/* navbar */}
+      <Navbar />
+      {/* main content */}
+      <main>
+        <HeroSection />
+      </main>
+    </div>
+  );
+};
